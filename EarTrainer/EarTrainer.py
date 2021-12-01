8fabb66ac5310ffa57f78a07e833d32af087cff2
jacob roberson <jaro6946@colorado.edu>
2021-11-30 21:07:11 -0500

    Add project files.

---
+++ EarTrainer/EarTrainer.py
@@ -0,0 +1,289 @@
+
+import PySimpleGUI as sg
+
+import time
+from mingus.midi.pyfluidsynth import fluidsynth
+from random import randrange
+import layouts
+import threading
+import random
+
+
+class stopNotes (threading.Thread):
+   def __init__(self):
+       threading.Thread.__init__(self)
+      
+   def run(self):
+       time.sleep(.25)
+       [fs.noteoff(0,x) for x in range(1,100)]
+
+
+#Initialize Fluidsynth
+
+fs = fluidsynth.Synth(gain=.8)
+fs.start(driver = 'dsound')
+
+sfid = fs.sfload("./SoundFonts/Guitar.sf2")
+fs.program_select(0, sfid, 0, 0)
+
+
+
+# Initalize the main loop
+memory=[0]
+intervals=['U','m2','M2','m3', 'M3', 'p4','t', 'p5', 'm6', 'M6', 'm7', 'M7', 'OCT']
+intervalChoices=[[0,0],[0,1],[0,2],[0,3], [0,4], [0,5],[0,6], [0,7],
+              [0,8], [0,9], [0,10], [0,11],[0,12]]
+currentGuess=0
+answerKey=[]
+
+seed=0
+chords=['majorChord', 'minorChord', 'major7Chord', 'dominate7Chord','minor7Chord','sus2Chord','sus4Chord']
+chordChoices=[[0, 4, 7], [0, 3, 7],[0, 4, 7, 11],[0, 4, 7, 10],
+             [0, 3, 7, 10],[0, 2, 7],[0, 5, 7]]
+
+degrees=['I','ii','iii','IV','V','vi','vii','OCT0']
+degreeChoices=[[0,4,7],[2,5,9],[4,7,11],[5,9,12],[7,11,14],[9,12,16],[11,14,17],[12,16,19]]
+
+majorScale=[0, 2, 4, 5, 7, 9, 11, 12]
+diatonicScale=[0,1,2,3,4,5,6,7,8,9,10,11,12]
+
+chordFlag=0
+playMode='majorScale'
+mode='quiz'
+
+window=layouts.homeLayout()
+
+while True:
+    seed=0
+    event, values = window.read(timeout=100)        # Poll every 100 ms
+    if event == 'Exit' or event == sg.WIN_CLOSED:
+        break
+    if event == '?':
+        print(memory, answerKey)
+    
+
+    # If a button was pressed, display it on the GUI by updating the text element
+    if event == 'play' or mode=='play':
+        if event=='play':
+            if mode=='play':
+                mode='quiz'
+                window.close()
+                window=layouts.homeLayout()
+                event, values = window.read(timeout=100) 
+            elif mode=='quiz':
+                mode='play'
+                degreeRanSeed=randrange(40,76-11)
+                window.close()
+                window=layouts.playLayout()
+                event, values = window.read(timeout=100) 
+    elif  event== 'majorScale':
+        playMode='majorScale'
+        window.close()
+        window=layouts.intervalViewLayout()
+        event, values = window.read(timeout=100) 
+    elif event == 'diatonicScale':
+        playMode='diatonicScale'
+        window.close()
+        window=layouts.intervalViewLayout()
+        event, values = window.read(timeout=100) 
+    elif event == 'chord':
+        playMode='chord'
+        window.close()
+        window=layouts.chordLayout()
+        event, values = window.read(timeout=100) 
+    
+    elif event == 'degree':
+        playMode ='degree'
+        window.close()
+        window=layouts.degreeLayout()
+        event, values = window.read(timeout=100) 
+
+
+    if event != sg.TIMEOUT_KEY:  
+        window['-OUTPUT-'].update(playMode)
+
+
+    for i,interval in enumerate(intervals): 
+            if interval==event:#oct
+                if mode=='play':
+                    playValue=i
+                    event='-exNEXT-'
+                    playMode='diatonicScale'
+                elif currentGuess>len(answerKey)-1:
+                    pass
+                elif event==answerKey[currentGuess]:
+                    print('Correct')
+                    currentGuess=currentGuess+1
+                    if currentGuess>len(answerKey)-1:
+                        print('You sexy!')
+                        currentGuess=100
+                else:
+                    print('Wrong')
+
+    for i,chord in enumerate(chords):
+            if chord==event:
+                if mode=='play':
+                    playValue=i
+                    event='-exNEXT-'
+                    playMode='chord'
+                elif currentGuess>len(answerKey)-1:
+                    pass
+                elif event==answerKey[currentGuess]:
+                    print('Correct')
+                    currentGuess=currentGuess+1
+                    if currentGuess>len(answerKey)-1:
+                        print('You sexy!')
+                else:
+                    print('wrong')
+
+
+    for i,chord in enumerate(degrees):
+            if chord==event:
+                if mode=='play':
+                    playValue=i
+                    event='-exNEXT-'
+                    playMode='degree'
+                elif currentGuess>len(answerKey)-1:
+                    pass
+                elif event==answerKey[currentGuess]:
+                    print('Correct')
+                    currentGuess=currentGuess+1
+                    if currentGuess>len(answerKey)-1:
+                        print('You sexy!')
+                else:
+                    print('wrong')
+
+
+    if event=='-exNEXT-':
+        mute=stopNotes()
+        strength=values[3]
+        chordOffset=values[2]/10
+        subListOffset=values[1]/10
+        #window['OUTPUT'].update('')
+        currentGuess=0
+        memory=[]
+        answerKey=[]
+        numberOfNotes=values[0]
+        
+        if playMode=='majorScale':                                        
+            ranSeed=randrange(40,76-majorScale[-1])
+            
+            noteList=list(majorScale)
+            for i,x in enumerate(majorScale):
+                noteList[i]=noteList[i]+ranSeed
+            #Choose apropriate number of notes
+            notes2Play=[[noteList[randrange(0,len(noteList))]] for i,j in enumerate(noteList) if i < numberOfNotes]
+
+            for note in range(1,len(notes2Play)):   
+                numericInterval=notes2Play[note][0]-notes2Play[note-1][0]
+                answerKey.append(intervals[abs(numericInterval)])
+            
+
+
+        if playMode=='diatonicScale': 
+            
+
+            ranSeed=randrange(40,76-diatonicScale[-1])
+            
+            noteList=list(diatonicScale)
+            for i,x in enumerate(diatonicScale):
+                noteList[i]=noteList[i]+ranSeed
+            #Choose apropriate number of notes
+            if mode=='play':
+                notes2Play=[[noteList[0]],[noteList[playValue]]] 
+                random.shuffle(notes2Play)
+            else:
+                notes2Play=[[noteList[randrange(0,len(noteList))]] for i,j in enumerate(noteList) if i < numberOfNotes]
+
+            for note in range(1,len(notes2Play)):   
+                numericInterval=notes2Play[note][0]-notes2Play[note-1][0]
+                answerKey.append(intervals[abs(numericInterval)])
+
+
+        if playMode=='chord':                                        
+            
+            chordChoices=[[0, 4, 7], [0, 3, 7],[0, 4, 7, 11],[0, 4, 7, 10],
+             [0, 3, 7, 10],[0, 2, 7],[0, 5, 7]]
+            chordList=chordChoices.copy()
+            for i,chord in enumerate(chordList):
+                ranSeed=randrange(40,76-11)
+                for j, note in enumerate(chord):
+                    chordList[i][j]=chordList[i][j]+ranSeed
+            #Choose apropriate number of notes
+            if mode=='play':
+                numberOfNotes=1
+                randomNotes=[playValue]
+                answerKey=[chords[playValue]]
+                notes2Play=[chordList[randomNotes[i]] for i,chord in enumerate(chordList) if i < numberOfNotes]
+
+            else:
+                randomNotes=[randrange(0,len(chordList)) for i,chord in enumerate(chordList) if i < numberOfNotes]
+                answerKey=[chords[randomNotes[i]] for i,chord in enumerate(chordList) if i < numberOfNotes]
+                notes2Play=[chordList[randomNotes[i]] for i,chord in enumerate(chordList) if i < numberOfNotes]
+
+
+        if playMode=='degree':                                        
+            
+            degreeChoices=[[0,4,7],[2,5,9],[4,7,11],[5,9,12],[7,11,14],[9,12,16],[11,14,17],[12,16,19]]
+            chordList=degreeChoices.copy()
+            
+            
+            #Choose apropriate number of notes
+
+            if mode=='play':
+                for i,chord in enumerate(chordList):
+                    for j, note in enumerate(chord):
+                        chordList[i][j]=chordList[i][j]+degreeRanSeed
+                numberOfNotes=1
+                randomNotes=[playValue]
+                answerKey=[degrees[playValue]]
+                notes2Play=[chordList[randomNotes[i]] for i,chord in enumerate(chordList) if i < numberOfNotes]
+
+            else:
+                ranSeed=randrange(40,76-11)
+                for i,chord in enumerate(chordList):
+                    for j, note in enumerate(chord):
+                        chordList[i][j]=chordList[i][j]+ranSeed
+                randomNotes=[randrange(0,len(chordList)) for i,chord in enumerate(chordList) if i < numberOfNotes]
+                answerKey=[degrees[randomNotes[i]] for i,chord in enumerate(chordList) if i < numberOfNotes]
+                notes2Play=[chordList[randomNotes[i]] for i,chord in enumerate(chordList) if i < numberOfNotes]
+
+                rootNote=[ranSeed]
+                notes2Play.insert(0,rootNote)
+       
+
+        memory=notes2Play
+        #accept list like this [[],[],[],[],[]].  Play notes simaltaneous in sublist, offset between sublist
+        for subList in notes2Play:
+            flag=0
+            for note in subList:
+                if note == subList[-1]:
+                    flag=1
+                fs.noteon(0,note,int(strength))
+                if flag==0:
+                    time.sleep(chordOffset)
+            time.sleep(subListOffset)
+        mute.start()
+
+
+    if event=='-exRESTART SONG-':
+        mute=stopNotes()
+        strength=values[3]
+        chordOffset=values[2]/10
+        subListOffset=values[1]/10
+        #window['OUTPUT'].update('')
+
+        for subList in notes2Play:
+            flag=0
+            for note in subList:
+                if note == subList[-1]:
+                    flag=1
+                fs.noteon(0,note,int(strength))
+                if flag==0:
+                    time.sleep(chordOffset)
+            time.sleep(subListOffset)
+        mute.start()
+        
+        
+
+            
---
+++ EarTrainer/layouts.py
@@ -0,0 +1,256 @@
+import PySimpleGUI as sg
+
+
+
+image_pause = './ButtonGraphics/Pause.png'
+image_restart = './ButtonGraphics/Restart.png'
+image_next = './ButtonGraphics/Next.png'
+image_exit = './ButtonGraphics/Exit.png'
+
+# Use the theme APIs to set the buttons to blend with background
+#sg.theme_button_color((sg.theme_background_color(), sg.theme_background_color()))
+#sg.theme_border_width(0)        # make all element flat
+
+
+def homeLayout():
+    layout= [[sg.Text('Media File Player',size=(17,1), font=("Helvetica", 25))],
+                [sg.Text(size=(15, 2), font=("Helvetica", 14), key='-OUTPUT-')],
+                [sg.Button(image_filename=image_restart, image_size=(50, 50), image_subsample=2,  key='-exRESTART SONG-'),
+                sg.Text(' ' * 2),
+                sg.Button('?',  font=("Helvetica", 12),size=(5, 2),  key='?'),
+                sg.Text(' ' * 2),
+                sg.Button(image_filename=image_next, image_size=(50, 50), image_subsample=2,  key='-exNEXT-'),
+                sg.Text(' ' * 2),
+                sg.Text(' ' * 2), sg.Button(image_filename=image_exit, image_size=(50, 50), image_subsample=2, key='Exit')],
+                [sg.Text('_'*30)],
+          
+            
+                [sg.Button('Diatonic Scale', font=("Helvetica", 12), size=(12, 1), key='diatonicScale'), 
+                 sg.Button('Major Scale', font=("Helvetica", 12), size=(10, 1), key='majorScale')], 
+                [sg.Button('chord', font=("Helvetica", 12), size=(8, 1), key='chord'),
+                 sg.Button('play', font=("Helvetica", 12), size=(8, 1), key='play'),
+                 sg.Button('degree', font=("Helvetica", 12), size=(8, 1), key='degree')],
+                 [sg.Text('_'*30)]
+                
+             
+                ]
+
+
+    # Open a form, note that context manager can't be used generally speaking for async forms
+    return sg.Window('Jacobs Interval Trainer', layout, default_element_size=(20, 1), font=("Helvetica", 25), grab_anywhere=True)
+
+
+def intervalViewLayout():
+    layout= [[sg.Text('Media File Player',size=(17,1), font=("Helvetica", 25))],
+                [sg.Text(size=(15, 2), font=("Helvetica", 14), key='-OUTPUT-')],
+                [sg.Button(image_filename=image_restart, image_size=(50, 50), image_subsample=2,  key='-exRESTART SONG-'),
+                sg.Text(' ' * 2),
+                sg.Button('?',  font=("Helvetica", 12),size=(5, 2),  key='?'),
+                sg.Text(' ' * 2),
+                sg.Button(image_filename=image_next, image_size=(50, 50), image_subsample=2,  key='-exNEXT-'),
+                sg.Text(' ' * 2),
+                sg.Text(' ' * 2), sg.Button(image_filename=image_exit, image_size=(50, 50), image_subsample=2, key='Exit')],
+                [sg.Text('_'*30)],
+          
+            
+                [sg.Button('Diatonic Scale', font=("Helvetica", 12), size=(12, 1), key='diatonicScale'), 
+                 sg.Button('Major Scale', font=("Helvetica", 12), size=(10, 1), key='majorScale')], 
+                [sg.Button('chord', font=("Helvetica", 12), size=(8, 1), key='chord'),
+                 sg.Button('play', font=("Helvetica", 12), size=(8, 1), key='play'),
+                 sg.Button('degree', font=("Helvetica", 12), size=(8, 1), key='degree')],
+                 [sg.Text('_'*30)],
+                [sg.Slider( range=(2, 10), default_value=2, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+                 sg.Text(' ' * 2),
+                 sg.Slider(range=(1, 100), default_value=10, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+                 sg.Text(' ' * 2),
+                 sg.Slider(range=(0, 100), default_value=10, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+                 sg.Text(' ' * 2),
+                 sg.Slider(range=(0, 100), default_value=99, size=(10, 20), orientation='vertical', font=("Helvetica", 15))
+                 #,sg.Output(size=(50,10), key='OUTPUT', font=("Helvetica", 8))
+                 ],
+                [sg.Text('# Notes', font=("Helvetica", 12), size=(7, 1)),
+                sg.Text('subListInterval', font=("Helvetica", 12), size=(12, 1)),
+                sg.Text('chordInterval', font=("Helvetica", 12), size=(9, 1)),
+                sg.Text('Volume', font=("Helvetica", 12), size=(8, 1))],
+                [sg.Text('_'*30)],
+                [sg.Button('U', font=("Helvetica", 12), size=(8, 1), key='U'), 
+                 sg.Button('m2', font=("Helvetica", 12), size=(8, 1), key='m2'), 
+                 sg.Button('M2', font=("Helvetica", 12), size=(8, 1), key='M2'),
+                 sg.Button('m3', font=("Helvetica", 12), size=(8, 1), key='m3'), 
+                 sg.Button('M3', font=("Helvetica", 12), size=(8, 1), key='M3'), 
+                 sg.Button('p4', font=("Helvetica", 12), size=(8, 1), key='p4'),
+                 sg.Button('t', font=("Helvetica", 12), size=(8, 1), key='t')], 
+                [sg.Button('p5', font=("Helvetica", 12), size=(8, 1), key='p5'), 
+                 sg.Button('m6', font=("Helvetica", 12), size=(8, 1), key='m6'), 
+                 sg.Button('M6', font=("Helvetica", 12), size=(8, 1), key='M6'),
+                 sg.Button('m7', font=("Helvetica", 12), size=(8, 1), key='m7'), 
+                 sg.Button('M7', font=("Helvetica", 12), size=(8, 1), key='M7'), 
+                 sg.Button('O', font=("Helvetica", 12), size=(8, 1), key='OCT')],
+                 [sg.Text('_'*30)]
+             
+                ]
+
+
+    # Open a form, note that context manager can't be used generally speaking for async forms
+    return sg.Window('Jacobs Interval Trainer', layout, default_element_size=(20, 1), font=("Helvetica", 25), grab_anywhere=True)
+
+
+def chordLayout():
+    layout= [[sg.Text('Media File Player',size=(17,1), font=("Helvetica", 25))],
+            [sg.Text(size=(15, 2), font=("Helvetica", 14), key='-OUTPUT-')],
+            [sg.Button(image_filename=image_restart, image_size=(50, 50), image_subsample=2,  key='-exRESTART SONG-'),
+            sg.Text(' ' * 2),
+            sg.Button('?',  font=("Helvetica", 12),size=(5, 2),  key='?'),
+            sg.Text(' ' * 2),
+            sg.Button(image_filename=image_next, image_size=(50, 50), image_subsample=2,  key='-exNEXT-'),
+            sg.Text(' ' * 2),
+            sg.Text(' ' * 2), sg.Button(image_filename=image_exit, image_size=(50, 50), image_subsample=2, key='Exit')],
+            [sg.Text('_'*30)],
+          
+            
+            [sg.Button('Diatonic Scale', font=("Helvetica", 12), size=(12, 1), key='diatonicScale'), 
+             sg.Button('Major Scale', font=("Helvetica", 12), size=(10, 1), key='majorScale')], 
+            [sg.Button('chord', font=("Helvetica", 12), size=(8, 1), key='chord'),
+             sg.Button('play', font=("Helvetica", 12), size=(8, 1), key='play'),
+             sg.Button('degree', font=("Helvetica", 12), size=(8, 1), key='degree')],
+             [sg.Text('_'*30)],
+            [sg.Slider( range=(1, 10), default_value=2, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+             sg.Text(' ' * 2),
+             sg.Slider(range=(1, 100), default_value=10, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+             sg.Text(' ' * 2),
+             sg.Slider(range=(0, 100), default_value=3, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+             sg.Text(' ' * 2),
+             sg.Slider(range=(0, 100), default_value=99, size=(10, 20), orientation='vertical', font=("Helvetica", 15))
+             #,sg.Output(size=(50,10), key='OUTPUT', font=("Helvetica", 8))
+             ],
+            [sg.Text('# Notes', font=("Helvetica", 12), size=(7, 1)),
+            sg.Text('subListInterval', font=("Helvetica", 12), size=(12, 1)),
+            sg.Text('chordInterval', font=("Helvetica", 12), size=(9, 1)),
+            sg.Text('Volume', font=("Helvetica", 12), size=(8, 1))],
+            [sg.Text('_'*30)],
+             [sg.Button('Major', font=("Helvetica", 12), size=(8, 1), key='majorChord'),
+             sg.Button('Minor', font=("Helvetica", 12), size=(8, 1), key='minorChord'), 
+             sg.Button('Major 7', font=("Helvetica", 12), size=(8, 1), key='major7Chord'), 
+             sg.Button('Minor 7', font=("Helvetica", 12), size=(8, 1), key='minor7Chord'),
+             sg.Button('Dominate 7', font=("Helvetica", 12), size=(10, 1), key='dominate7Chord'),
+             sg.Button('Sus2', font=("Helvetica", 12), size=(8, 1), key='sus2Chord'),
+             sg.Button('Sus4', font=("Helvetica", 12), size=(8, 1), key='sus4Chord')],
+             [sg.Text('_'*30)]
+             
+            ]
+
+
+# Open a form, note that context manager can't be used generally speaking for async forms
+    return sg.Window('Jacobs Interval Trainer', layout, default_element_size=(20, 1), font=("Helvetica", 25), grab_anywhere=True)
+
+
+def degreeLayout():
+    layout= [[sg.Text('Media File Player',size=(17,1), font=("Helvetica", 25))],
+            [sg.Text(size=(15, 2), font=("Helvetica", 14), key='-OUTPUT-')],
+            [sg.Button(image_filename=image_restart, image_size=(50, 50), image_subsample=2,  key='-exRESTART SONG-'),
+            sg.Text(' ' * 2),
+            sg.Button('?',  font=("Helvetica", 12),size=(5, 2),  key='?'),
+            sg.Text(' ' * 2),
+            sg.Button(image_filename=image_next, image_size=(50, 50), image_subsample=2,  key='-exNEXT-'),
+            sg.Text(' ' * 2),
+            sg.Text(' ' * 2), sg.Button(image_filename=image_exit, image_size=(50, 50), image_subsample=2, key='Exit')],
+            [sg.Text('_'*30)],
+          
+            
+            [sg.Button('Diatonic Scale', font=("Helvetica", 12), size=(12, 1), key='diatonicScale'), 
+             sg.Button('Major Scale', font=("Helvetica", 12), size=(10, 1), key='majorScale')], 
+            [sg.Button('chord', font=("Helvetica", 12), size=(8, 1), key='chord'),
+             sg.Button('play', font=("Helvetica", 12), size=(8, 1), key='play'),
+             sg.Button('degree', font=("Helvetica", 12), size=(8, 1), key='degree')],
+             [sg.Text('_'*30)],
+            [sg.Slider( range=(1, 10), default_value=2, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+             sg.Text(' ' * 2),
+             sg.Slider(range=(1, 100), default_value=10, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+             sg.Text(' ' * 2),
+             sg.Slider(range=(0, 100), default_value=3, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+             sg.Text(' ' * 2),
+             sg.Slider(range=(0, 100), default_value=99, size=(10, 20), orientation='vertical', font=("Helvetica", 15))
+             #,sg.Output(size=(50,10), key='OUTPUT', font=("Helvetica", 8))
+             ],
+            [sg.Text('# Notes', font=("Helvetica", 12), size=(7, 1)),
+            sg.Text('subListInterval', font=("Helvetica", 12), size=(12, 1)),
+            sg.Text('chordInterval', font=("Helvetica", 12), size=(9, 1)),
+            sg.Text('Volume', font=("Helvetica", 12), size=(8, 1))],
+            [sg.Text('_'*30)],
+            
+             [sg.Button('I', font=("Helvetica", 12), size=(8, 1), key='I'),
+             sg.Button('ii', font=("Helvetica", 12), size=(8, 1), key='ii'), 
+             sg.Button('iii', font=("Helvetica", 12), size=(8, 1), key='iii'), 
+             sg.Button('IV', font=("Helvetica", 12), size=(8, 1), key='IV'),
+             sg.Button('V', font=("Helvetica", 12), size=(10, 1), key='V'),
+             sg.Button('vi', font=("Helvetica", 12), size=(8, 1), key='vi'),
+             sg.Button('vii', font=("Helvetica", 12), size=(8, 1), key='vii'),
+             sg.Button('O', font=("Helvetica", 12), size=(8, 1), key='OCT0')],
+             [sg.Text('_'*30)]
+             
+            ]
+
+
+    # Open a form, note that context manager can't be used generally speaking for async forms
+    return sg.Window('Jacobs Interval Trainer', layout, default_element_size=(20, 1), font=("Helvetica", 25), grab_anywhere=True)
+
+
+
+def playLayout():
+    layout= [[sg.Text('Media File Player',size=(17,1), font=("Helvetica", 25))],
+            [sg.Text(size=(15, 2), font=("Helvetica", 14), key='-OUTPUT-')],
+            [sg.Button('?',  font=("Helvetica", 12),size=(5, 2),  key='?'),
+            sg.Text(' ' * 2), sg.Button(image_filename=image_exit, image_size=(50, 50), image_subsample=2, key='Exit')],
+            [sg.Text('_'*30)],
+          
+            [sg.Button('home', font=("Helvetica", 12), size=(8, 1), key='play')],
+             [sg.Text('_'*30)],
+            [sg.Slider( range=(1, 10), default_value=2, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+             sg.Text(' ' * 2),
+             sg.Slider(range=(1, 100), default_value=10, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+             sg.Text(' ' * 2),
+             sg.Slider(range=(0, 100), default_value=3, size=(10, 20), orientation='vertical', font=("Helvetica", 15)),
+             sg.Text(' ' * 2),
+             sg.Slider(range=(0, 100), default_value=99, size=(10, 20), orientation='vertical', font=("Helvetica", 15))
+             #,sg.Output(size=(50,10), key='OUTPUT', font=("Helvetica", 8))
+             ],
+            [sg.Text('# Notes', font=("Helvetica", 12), size=(7, 1)),
+            sg.Text('subListInterval', font=("Helvetica", 12), size=(12, 1)),
+            sg.Text('chordInterval', font=("Helvetica", 12), size=(9, 1)),
+            sg.Text('Volume', font=("Helvetica", 12), size=(8, 1))],
+            [sg.Text('_'*30)],
+            [sg.Button('U', font=("Helvetica", 12), size=(8, 1), key='U'), 
+             sg.Button('m2', font=("Helvetica", 12), size=(8, 1), key='m2'), 
+             sg.Button('M2', font=("Helvetica", 12), size=(8, 1), key='M2'),
+             sg.Button('m3', font=("Helvetica", 12), size=(8, 1), key='m3'), 
+             sg.Button('M3', font=("Helvetica", 12), size=(8, 1), key='M3'), 
+             sg.Button('p4', font=("Helvetica", 12), size=(8, 1), key='p4'),
+             sg.Button('t', font=("Helvetica", 12), size=(8, 1), key='t')], 
+            [sg.Button('p5', font=("Helvetica", 12), size=(8, 1), key='p5'), 
+             sg.Button('m6', font=("Helvetica", 12), size=(8, 1), key='m6'), 
+             sg.Button('M6', font=("Helvetica", 12), size=(8, 1), key='M6'),
+             sg.Button('m7', font=("Helvetica", 12), size=(8, 1), key='m7'), 
+             sg.Button('M7', font=("Helvetica", 12), size=(8, 1), key='M7'), 
+             sg.Button('O', font=("Helvetica", 12), size=(8, 1), key='OCT')],
+             [sg.Button('Major', font=("Helvetica", 12), size=(8, 1), key='majorChord'),
+             sg.Button('Minor', font=("Helvetica", 12), size=(8, 1), key='minorChord'), 
+             sg.Button('Major 7', font=("Helvetica", 12), size=(8, 1), key='major7Chord'), 
+             sg.Button('Minor 7', font=("Helvetica", 12), size=(8, 1), key='minor7Chord'),
+             sg.Button('Dominate 7', font=("Helvetica", 12), size=(10, 1), key='dominate7Chord'),
+             sg.Button('Sus2', font=("Helvetica", 12), size=(8, 1), key='sus2Chord'),
+             sg.Button('Sus4', font=("Helvetica", 12), size=(8, 1), key='sus4Chord')],
+             [sg.Button('I', font=("Helvetica", 12), size=(8, 1), key='I'),
+             sg.Button('ii', font=("Helvetica", 12), size=(8, 1), key='ii'), 
+             sg.Button('iii', font=("Helvetica", 12), size=(8, 1), key='iii'), 
+             sg.Button('IV', font=("Helvetica", 12), size=(8, 1), key='IV'),
+             sg.Button('V', font=("Helvetica", 12), size=(10, 1), key='V'),
+             sg.Button('vi', font=("Helvetica", 12), size=(8, 1), key='vi'),
+             sg.Button('vii', font=("Helvetica", 12), size=(8, 1), key='vii'),
+             sg.Button('O', font=("Helvetica", 12), size=(8, 1), key='OCT')],
+             [sg.Text('_'*30)]
+             
+            ]
+
+
+    # Open a form, note that context manager can't be used generally speaking for async forms
+    return sg.Window('Jacobs Interval Trainer', layout, default_element_size=(20, 1), font=("Helvetica", 25), grab_anywhere=True)
